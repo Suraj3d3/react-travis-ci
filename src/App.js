ae44352246b86ae3f539b657f15dde4c658c3cb6
suraj <suraj3d3@gmail.com>
2022-09-01 20:38:05 +0530

    change in statement

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test statement
+          Testing github action CI/CD workflow
         </a>
       </header>
     </div>
